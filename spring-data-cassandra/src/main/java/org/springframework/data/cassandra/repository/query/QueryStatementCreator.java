390b05bd13074432faa14e4b7dbd7343ab8678ca
Humberd <humberd2@gmail.com>
2018-12-04 20:28:10 +0100

    Added query statement creator

--- spring-data-cassandra/src/main/java/org/springframework/data/cassandra/repository/query/QueryStatementCreator.java
+++ spring-data-cassandra/src/main/java/org/springframework/data/cassandra/repository/query/QueryStatementCreator.java
@@ -83,6 +83,30 @@ class QueryStatementCreator {
 	}
 
 	/**
+	 * Create a {@literal DELETE} {@link Statement} from a {@link PartTree} and apply query options.
+	 *
+	 * @param statementFactory must not be {@literal null}.
+	 * @param tree must not be {@literal null}.
+	 * @param parameterAccessor must not be {@literal null}.
+	 * @return the {@literal DELETE} {@link Statement}.
+	 */
+	Statement delete(StatementFactory statementFactory, PartTree tree,
+					 CassandraParameterAccessor parameterAccessor) {
+		Function<Query, Statement> function = query -> {
+
+			RegularStatement statement = statementFactory.delete(query, requirePersistentEntity());
+
+			if (LOG.isDebugEnabled()) {
+				LOG.debug(String.format("Created query [%s]/", statement));
+			}
+
+			return statement;
+		};
+
+		return doWithQuery(parameterAccessor, tree, function);
+	}
+
+	/**
 	 * Create a {@literal COUNT} {@link Statement} from a {@link PartTree} and apply query options.
 	 *
 	 * @param statementFactory must not be {@literal null}.
